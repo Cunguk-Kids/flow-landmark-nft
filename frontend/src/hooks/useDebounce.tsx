994ef8448c2b32892cd8ca84e05c00533700503a
harkon666 <bryan.wicaksanaa@gmail.com>
2025-11-22 14:04:34 +0700

    add useDebounce Utils

---
+++ frontend/src/hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    // Set timeout untuk mengubah value setelah delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cleanup timeout jika value berubah sebelum delay selesai
+    // (Ini yang mencegah request beruntun)
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
